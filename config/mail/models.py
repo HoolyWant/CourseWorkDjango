c7d5f830aeb87920c4c63f64c0fa8df137781a25
hoolywant <sir.saltickow@yandex.ru>
2023-09-02 17:12:11 +0400

    first version of models is done

--- config/mail/models.py
+++ config/mail/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class ClientService(models.Model):
+    contact_email = models.CharField(max_length=100, verbose_name='контактная почта')
+    full_name = models.CharField(max_length=100, verbose_name='ФИО')
+    comment = models.TextField(verbose_name='ФИО', **NULLABLE)
+
+
+
+class MailDistribution(models.Model):
+    PERIOD_DAILY = 'daily'
+    PERIOD_WEEKLY = 'weekly'
+    PERIOD_MONTHLY = 'monthly'
+
+    PERIODS = ((PERIOD_DAILY, 'Ежедневно'),
+               (PERIOD_WEEKLY, 'Еженедельно'),
+               (PERIOD_MONTHLY, 'Ещемесячно'))
+
+    STATUS_CREATED = 'created'
+    STATUS_STARTED = 'started'
+    STATUS_DONE = 'done'
+
+    STATUSES = ((STATUS_CREATED, 'Запущена'),
+                (STATUS_STARTED, 'Создана'),
+                (STATUS_DONE, 'Завершена'))
+    date_start = models.DateTimeField(verbose_name='время начала рассылки')
+    date_finish = models.DateTimeField(verbose_name='время окончания рассылки')
+    period = models.CharField(max_length=20, choices=PERIODS, default=PERIOD_DAILY, verbose_name='период')
+    distribution_status = models.CharField(max_length=20,
+                                           choices=STATUSES,
+                                           default=STATUS_CREATED,
+                                           verbose_name='статус')
+    message = models.ForeignKey('MessagesForDistribution',
+                                on_delete=models.CASCADE,
+                                verbose_name='сообшение',
+                                **NULLABLE)
+
+
+class MessagesForDistribution(models.Model):
+    message_theme = models.CharField(max_length=100, verbose_name='тема письма')
+    message_body = models.TextField(verbose_name='текст письма')
+
+
+class Logs(models.Model):
+    last_attempt = models.DateTimeField(verbose_name='время последней попытки')
+    attempt_status = models.BooleanField(default=False, verbose_name='статус попытки')
+    mail_response = models.BooleanField(default=False, verbose_name='ответ почтового сервиса')
+
+
+
+
