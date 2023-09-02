fe0471e79e1cd35acb9edde4a4a79f18192d183e
hoolywant <sir.saltickow@yandex.ru>
2023-09-02 18:39:53 +0400

    created controllers

--- config/mail/views.py
+++ config/mail/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView
 
-# Create your views here.
+from config.mail.models import MessagesForDistribution, MailDistributionSettings, Logs, Client
+
+
+class MessagesList(ListView):  # просмотр списка рассылок
+    model = MessagesForDistribution
+
+
+class MessageDetail(DetailView):  # просмотр рассылки
+    model = MessagesForDistribution
+
+
+class MessageCreate(CreateView):  # создания рассылки с настройками
+    model = MailDistributionSettings
+    fields = ('message_theme', 'message_body',
+              'date_start', 'date_finish',
+              'period')
+    success_url = reverse_lazy('mail:messages_list')
+
+
+class ClientCreate(CreateView):  # созданиt клиента
+    model = Client
+    fields = ('full_name', 'contact_email', 'comment')
+    success_url = reverse_lazy('mail:list')
+
+
+class ClientDetail(DetailView):  # просмотра клиента
+    model = Client
+
+
+class ClientsList(ListView):  # просмотр списка клиентов
+    model = Client
+
+
+class LogDetail(DetailView):  # просмотр логов
+    model = Logs
